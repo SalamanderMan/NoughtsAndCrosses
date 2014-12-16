49c960d78ba04fb2115354e0e03e14e4a5b74999
SalamanderMan <andrew.welsh@tombola-498.local>
2014-12-16 17:18:49 +0000

    Started working on player choices. The number doesn't change  yet however.

--- main-app/app/scripts/services/game-model.js
+++ main-app/app/scripts/services/game-model.js
@@ -1,17 +1,36 @@
-(function () {
+(function () {'use strict';
     angular.module('noughtsAndCrossesApp')
     .service('gameModel', function () {
 
         this.outcome = 'continue';
         this.gameState = '000000001';
         this.winner = 0;
-        this.player1 = 'random';
-        this.player2 = 'random';
-
+        this.player1 = 'human';
+        this.player2 = 'human';
+        this.currentPlayer = 1;
         this.newGame = function () {
             //TODO: Call API Wrapper to make service call
         };
 
-        //todo: make move.....
+            var cyclePlayerChoice = function (currentPlayer){
+            if(currentPlayer === 'human') { return 'random';}
+            else if (currentPlayer === 'random') {return 'pre-trained';}
+                return 'human';
+
+            };
+
+            this.togglePlayerChoice1 = function () {
+                var me = this;
+                me.player1 = cyclePlayerChoice(me.player1);
+
+            };       //todo: make move.....
+
+            this.togglePlayerChoice2 = function () {
+                var me = this;
+                me.player2 = cyclePlayerChoice(me.player2);
+
+            };
     });
 }());
+
+
