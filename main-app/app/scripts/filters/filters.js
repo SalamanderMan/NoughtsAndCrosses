4a4168a1b4aabe934e896b18e2b5df9ab030d7d0
SalamanderMan <andrew.welsh@tombola-498.local>
2014-12-22 13:56:31 +0000

    Client now works to play out a human vs random game.

--- main-app/app/scripts/filters/filters.js
+++ main-app/app/scripts/filters/filters.js
@@ -1,8 +1,16 @@
 angular.module('noughtsAndCrossesApp')
     .filter('playerNumberToGamePiece', function () {
-        return function () {
-            return 'X';
-
+        return function (input) {
+            if (input === '1') {
+                return 'X';
+            }else if (input === '2') {
+                return 'O';
+            }
+            return  '';
         };
-    }
-);
+    });
+
+
+
+
+
