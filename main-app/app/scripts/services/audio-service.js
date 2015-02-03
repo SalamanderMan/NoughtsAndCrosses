463f1f62f3c5e72d7489c6b4fedff71ccff2d9d5
SalamanderMan <andrew.welsh@tombola-539.local>
2015-02-03 15:31:08 +0000

    Audio sprites now working fully.

--- main-app/app/scripts/services/audio-service.js
+++ main-app/app/scripts/services/audio-service.js
@@ -4,37 +4,28 @@
             var audioElement;
 
             var setUpAudioSprite = function(){
-
                 var document = $document[0];
                 audioElement = document.createElement('audio');
-
             };
 
             this.newGame = function() {
-              
-                defaultSpriteValues();
-
+                playSprite(2.5, 3200);
             };
 
             this.makeMove = function (){
-
-                defaultSpriteValues();
-
+                playSprite(0.8, 1000);
             };
 
-
-
-            var defaultSpriteValues = function () {
+            var playSprite = function (startTime, duration) {
                 var source = document.createElement('source');
                 source.src = 'sounds/audio-sprite.mp3';
                 source.type = 'audio/mpeg';
                 audioElement.appendChild(source);
                 document.body.appendChild(audioElement);
-
-                audioElement.currentTime = 3.3;
+                audioElement.currentTime = startTime;
                 setTimeout(function(){
                     audioElement.pause ();
-                },3300);
+                },duration);
                 audioElement.play();
             };
 
